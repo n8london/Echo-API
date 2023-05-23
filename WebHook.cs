91e81237e84fe540118806d82ba79f2cbcc5942f
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-05-23 14:43:50 +0200

    initial commit

---
+++ WebHook.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace GBelenky.WebHook
+{
+    public class WebHook
+    {
+        private readonly ILogger _logger;
+
+        public WebHook(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<WebHook>();
+        }
+
+        [Function("WebHook")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            //read payload into string
+            string? payload = req.ReadAsString();
+            _logger.LogInformation($"Event payload: {payload}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+            response.WriteString(payload);
+
+            return response;
+        }
+    }
+}
