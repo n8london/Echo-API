fbc26ca849fddc4311510a142ed46f52e5a28e32
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-05-25 18:31:43 +0200

    gitignore

--- WebHook.cs
+++ WebHook.cs
@@ -15,7 +15,7 @@ namespace GBelenky.WebHook
         }
 
         [Function("WebHook")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", "options")] HttpRequestData req)
         {
             //read payload into string
             string? payload = req.ReadAsString();
